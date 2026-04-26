7c712ea10ebc84c346f4e191bf1e0cd47e437545
Test <test@Tests-MacBook-Air.local>
2026-04-26 06:23:10 +0200

    fix landing and app routes

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'landing.html')));
+app.get('/app', (req, res) => res.sendFile(path.join(__dirname, 'public', 'app.html')));
 
 const DB = path.join(__dirname, 'data', 'tenders.json');
 
